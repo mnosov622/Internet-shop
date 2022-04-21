327cf490be4d4355611f4fe54bc47d8604f8f5f1
Max N <smth@gmail.com>
2022-04-21 11:09:52 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.nav-list-holder').classList.toggle('nav-list-holder--visible');
+}
